7abd431449e89d15e6996e06bb06cf0d6d00db30
akki2332 <akashsoam123456789@gmail.com>
2026-04-11 10:05:42 +0530

    changes

--- backend/Routes/predict.routes.js
+++ backend/Routes/predict.routes.js
@@ -3,6 +3,6 @@ import {protectRoute} from '../middleware/auth.middleware.js'
 import {predict} from '../Controller/predict.controller.js'
 const router=Router();
 
-router.get('/',protectRoute,predict);
+router.post('/',protectRoute,predict);
 
 export default router
